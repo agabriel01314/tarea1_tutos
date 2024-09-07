2b123066e31fbcb8d0beecdda36d41763d02ca77
agabriel01314 <xailuz4@gmail.com>
2024-09-07 14:53:24 -0600

    se agrega el resto del codigo

--- tarea1.java
+++ tarea1.java
@@ -9,3 +9,27 @@ public class tarea1 {
         System.out.println("Hay " + count + " números Machete menores a 10,000.");
     }
     
+    public static boolean isMachete(int number) {
+        int temp = number;
+        for (int i = 0; i < 50; i++) {
+            temp += reverseNumber(temp);
+            if (isPalindrome(temp)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int reverseNumber(int number) {
+        int reversed = 0;
+        while (number > 0) {
+            reversed = (reversed * 10) + (number % 10);
+            number /= 10;
+        }
+        return reversed;
+    }
+
+    public static boolean isPalindrome(int number) {
+        return number == reverseNumber(number);
+    }
+}
