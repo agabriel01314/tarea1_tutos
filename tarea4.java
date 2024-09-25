4dde9d1d9d81c0cbb69426e5d7f9a3fd0351853c
agabriel01314 <xailuz4@gmail.com>
2024-09-25 11:05:22 -0600

    se agregam los roman map

---
+++ tarea4.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class tarea4 {
+    
+    public static int romanToInt(String s) {
+        HashMap<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
