6087c5dfc0a768285cba686bc1d8e1d9df247179
agabriel01314 <xailuz4@gmail.com>
2024-09-25 11:18:23 -0600

    se agrega el resto del codigo de la tarea 4

--- tarea4.java
+++ tarea4.java
@@ -12,3 +12,32 @@ public class tarea4 {
         romanMap.put('D', 500);
         romanMap.put('M', 1000);
 
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int current = romanMap.get(s.charAt(i));
+
+            if (i < s.length() - 1 && current < romanMap.get(s.charAt(i + 1))) {
+                result -= current;
+            } else {
+                result += current;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String roman1 = "III";       
+        String roman2 = "IV";        
+        String roman3 = "IX";        
+        String roman4 = "LVIII";     
+        String roman5 = "MCMXCIV";   
+
+        System.out.println(roman1 + " => " + romanToInt(roman1));
+        System.out.println(roman2 + " => " + romanToInt(roman2));
+        System.out.println(roman3 + " => " + romanToInt(roman3));
+        System.out.println(roman4 + " => " + romanToInt(roman4));
+        System.out.println(roman5 + " => " + romanToInt(roman5));
+    }
+}
